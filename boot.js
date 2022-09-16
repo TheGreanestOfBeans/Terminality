f7469801582dc8133493946f296fe8c4b8874730
wimo135 <ebdn2017@Gmail.com>
2022-09-16 18:41:43 -0400

    New boot message

--- boot.js
+++ boot.js
@@ -1,4 +1,5 @@
 function boot(){
     Print("Welcome to Terminality v1.0"); 
     Print("This terminal is for internal use only! If you are not a SATCOMP employee log off immediately!");
+    Print("<br>Type `help` for a list of commands");
 }
